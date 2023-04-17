36f2ac8b24ea54e3472638579e4c50baf82ddc1b
adecel <rustymabiala150@gmail.com>
2023-04-17 14:56:16 +0200

    first interation, i added 2 files and modified 2

---
+++ resources/views/web_services.blade.php
@@ -0,0 +1,331 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Vsoft Systems</title>
+    <meta name="description" content="" />
+    <meta name="keywords" content="" />
+    <meta name="author" content="vsoftsystems" />
+
+    <link rel="shortcut icon" href="images/favicon.ico">
+
+    <!-- Bootstrap core CSS -->
+    <link rel="stylesheet" href="css/bootstrap.min.css" type="text/css">
+
+    <!-- Magnificpopup Css -->
+    <link rel="stylesheet" type="text/css" href="css/magnific-popup.css">
+
+    <!--Material Icon -->
+    <link rel="stylesheet" type="text/css" href="css/materialdesignicons.min.css" />
+
+    <!--Font Awesome -->
+    <link rel="stylesheet" type="text/css" href="css/font-awesome.min.css" />
+
+    <!--Slider-->
+    <link rel="stylesheet" href="css/owl.carousel.css"/>
+    <link rel="stylesheet" href="css/owl.theme.css"/>
+    <link rel="stylesheet" href="css/owl.transitions.css"/>
+
+    <!-- Custom  Css -->
+    <link rel="stylesheet" type="text/css" href="css/style.css" />
+
+</head>
+
+<body>
+<!-- NAVBAR -->
+<nav class="navbar navbar-expand-lg fixed-top navbar-custom sticky">
+    <div class="container">
+        <a class="navbar-brand logo" href="#">Vsoft Systems</a>
+        <button class="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <i class="mdi mdi-menu"></i>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#home">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#service">Services</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#portfolio">Portfolio</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#client">Clients</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#team">Team</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#articles">Articles</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#price">Store</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#contact">Contact</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
+<!-- NAVBAR END-->
+
+<!-- HOME START-->
+<section class="bg-home-half" id="home">
+    <div class="bg-overlay"></div>
+    <div class="home-center">
+        <div class="home-desc-center">
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-lg-9">
+                        <div class="text-left text-white">
+                            <!-- <h5 class="home-small-title text-uppercase text-white-50 mb-4">We create the web</h5> -->
+                            <h2 class="home-title mb-4">Web Services</h2>
+                            <!-- <p class="home-desc text-white-50 mx-auto">Maecenas class semper class semper sollicitudin lectus lorem iaculis imperdiet aliquam vehicula tempor auctor curabitur pede aenean ornare.</p> -->
+                            <div class="text-left pt-4 navbar-nav" style="width: 250px">
+                                <a href="#feature" class="btn btn-custom"> More <i class="mdi mdi-arrow-down ml-2"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- HOME END-->
+
+{{-- Floating Action Button #Fab--}}
+<x-fab/>
+
+<!-- WELCOME START -->
+<section class="section" id="feature">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-lg-7">
+                <div class="text-center">
+                    <h4 class="title-heading">web_services!!!!</h4>
+                    <p class="title-desc text-muted mt-3">Here goes the small description about "web_services"</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="row mt-4">
+            <div class="col-lg-4">
+                <a href="/website_design">
+                    <div class="features mt-4">
+                        <div class="text-center">
+                            <div class="mb-4">
+                                <img src="images/about/2.jpg" class="img-fluid rounded" alt="">
+                            </div>
+
+                            <h4>Website Design</h4>
+                            <p class="features-desc text-muted mt-3 pl-2 pr-2">Finding a professional web design company is a tiresome job so many to choose from !</p>
+                        </div>
+                    </div>
+                </a>
+            </div>
+
+            <div class="col-lg-4">
+                <div class="features mt-4">
+                    <div class="text-center">
+                        <div class="mb-4">
+                            <img src="images/about/2.jpg" class="img-fluid rounded" alt="">
+                        </div>
+                        <h4>Web Maintenance</h4>
+                        <p class="features-desc text-muted mt-3 pl-2 pr-2">We at Vsoft Systems, with our website maintenance team we will focus on enhancing existing</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-lg-4">
+                <div class="features mt-4">
+                    <div class="text-center">
+                        <div class="mb-4">
+                            <img src="images/about/3.jpg" class="img-fluid rounded" alt="">
+                        </div>
+                        <h4>Web Development</h4>
+                        <p class="features-desc text-muted mt-3 pl-2 pr-2">VsoftSytems offers a professional Website development service to help you successfully plan,</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="row mt-4">
+            <div class="col-lg-4">
+                <div class="features mt-4">
+                    <div class="text-center">
+                        <div class="mb-4">
+                            <img src="images/about/2.jpg" class="img-fluid rounded" alt="">
+                        </div>
+
+                        <h4>Graphic Design & Logo Service</h4>
+                        <p class="features-desc text-muted mt-3 pl-2 pr-2">Graphic design is the process of communicating visually using text and images to present information.</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-lg-4">
+                <div class="features mt-4">
+                    <div class="text-center">
+                        <div class="mb-4">
+                            <img src="images/about/2.jpg" class="img-fluid rounded" alt="">
+                        </div>
+                        <h4>Search Engine Optimization</h4>
+                        <p class="features-desc text-muted mt-3 pl-2 pr-2">Vsoft Systems is a Cape Town based company. We offer and are actively engaged in SEO ( search engine optimization ) services for our clients.</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-lg-4">
+                <div class="features mt-4">
+                    <div class="text-center">
+                        <div class="mb-4">
+                            <img src="images/about/3.jpg" class="img-fluid rounded" alt="">
+                        </div>
+                        <h4>(PPC) Pay-Per-Click</h4>
+                        <p class="features-desc text-muted mt-3 pl-2 pr-2">We value collaboration, innovation, and a strong work ethic. We believe in building strong partnerships with our clients, staying on the cutting edge of technological developments, and going the extra mil</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</section>
+<!-- WELCOME END -->
+
+<!-- SERVICES START -->
+
+<!-- SERVICES END -->
+
+<!-- PORTFOLIO START-->
+
+
+        <!-- portfolio menu -->
+
+        <!-- End portfolio  -->
+
+    <!-- Gallary -->
+
+<!-- PORTFOLIO END-->
+
+<!-- CLIENT START -->
+
+<!-- CLIENT END -->
+
+<!-- TEAM START -->
+
+<!-- TEAM END -->
+
+<!-- START CTA -->
+<!-- <section class="section bg-cta">
+    <div class="bg-overlay"></div>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-lg-8">
+                <h3 class="text-white text-center">Ready to start your next
+                    web project now?</h3>
+                <p class="cta-desc text-white text-center mt-3">When a good idea comes, you know, part of my job is to move it around, just see what people think, get people talking about it.</p>
+                <div class="text-center mt-4">
+                    <a href="#" class="btn btn-custom-white">Get Started</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</section> -->
+<!-- END CTA -->
+
+<!-- BLOG START -->
+
+<!-- BLOG END -->
+
+<!-- CONTACT START -->
+
+<!-- CONTACT END -->
+
+<!-- FOOTER START -->
+<section class="footer bg-dark">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-4">
+                <div class="footer-menu">
+                    <h5 class="mb-4 text-uppercase">VSoft Systems</h5>
+                    <p>The Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium eaque quae ab illo inventore veritatis et.</p>
+                    <div class="text-white">
+                        <a href="#facebook"><i class="mdi mdi-facebook facebook footer_mdi"></i></a>
+                        <a href="#twitter"><i class="mdi mdi-twitter twitter footer_mdi"></i></a>
+                        <a href="#google"><i class="mdi mdi-google google footer_mdi"></i></a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-lg-2">
+                <div class="footer-menu">
+                    <h5 class="mb-4 text-uppercase">About</h5>
+                    <ul class="text-muted list-unstyled">
+                        <li><a href="#">About Us</a></li>
+                        <li><a href="#">Press</a></li>
+                        <li><a href="#">Job Opening</a></li>
+                        <li><a href="#">Term</a></li>
+                        <li><a href="#">Privacy</a></li>
+                    </ul>
+                </div>
+            </div>
+
+            <div class="col-lg-2">
+                <div class="footer-menu">
+                    <h5 class="mb-4 text-uppercase">Help Center</h5>
+                    <ul class="text-muted list-unstyled">
+                        <li><a href="#">Accounting</a></li>
+                        <li><a href="#">Billing</a></li>
+                        <li><a href="#">General Question</a></li>
+                    </ul>
+                </div>
+            </div>
+
+            <div class="col-lg-4">
+                <div class="footer-menu">
+                    <h5 class="mb-4 text-uppercase">Contact us</h5>
+                    <p>The Nam eget dui Etiam rhoncus. Maecenas eget sem quam semper libero.</p>
+                    <span class="footer-support">Support@abc.com</span>
+                </div>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="text-center mt-5">
+                    <p class="mb-0">2020  ©  VSoft Systems.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- FOOTER END -->
+
+<!-- JAVASCRIPTS -->
+<script src="js/jquery.min.js"></script>
+<!-- SCROLL -->
+<script src="js/scrollspy.min.js"></script>
+<!-- SCROLL -->
+<script src="js/feather.min.js"></script>
+<!--  -->
+<!-- bootstrap -->
+<script src="js/bootstrap.bundle.min.js"></script>
+<!-- easing -->
+<script src="js/jquery.easing.min.js"></script>
+<!-- Portfolio -->
+<script src="js/jquery.magnific-popup.min.js"></script>
+<script src="js/isotope.js"></script>
+<!-- scroll -->
+<script src="js/scrollspy.min.js"></script>
+<!-- Counter -->
+<script src="js/jquery.counterup.min.js"></script>
+<!-- Owl Carousel -->
+<script src="js/owl.carousel.min.js"></script>
+<!-- Custom -->
+<script src="js/app.js"></script>
+
+</body>
+</html>
