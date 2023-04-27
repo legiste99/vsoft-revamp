46ebe4e798b64b883956924ec82450b5498353de
Thapelo509 <81172568+Thapelo509@users.noreply.github.com>
2023-04-27 18:41:18 +0200

    changes

---
+++ resources/views/services/technology_expertise/ms_with_health_&_safety.blade.php
@@ -0,0 +1,273 @@
+<!DOCTYPE html>
+<html>
+<head>
+<x-head/>
+</head>
+
+<body>
+<!-- NAVBAR -->
+<nav class="navbar navbar-expand-lg fixed-top navbar-custom sticky">
+    <div class="container">
+        <a class="navbar-brand logo" href="#">Vsoft Systems</a>
+        <button class="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <i class="mdi mdi-menu"></i>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#home">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#service">Services</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#portfolio">Portfolio</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#client">Clients</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#team">Team</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#articles">Articles</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#price">Store</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#contact">Contact</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
+<!-- NAVBAR END-->
+
+<!-- HOME START-->
+<section class="bg-home-half" id="home">
+    <div class="bg-overlay"></div>
+    <div class="home-center">
+        <div class="home-desc-center">
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-lg-9">
+                        <div class="text-left text-white">
+                            <!-- <h5 class="home-small-title text-uppercase text-white-50 mb-4">We create the web</h5> -->
+                            <h2 class="home-title mb-4">Mining Solution with health & safety</h2>
+                            <!-- <p class="home-desc text-white-50 mx-auto">Maecenas class semper class semper sollicitudin lectus lorem iaculis imperdiet aliquam vehicula tempor auctor curabitur pede aenean ornare.</p> -->
+                            <div class="text-left pt-4 navbar-nav" style="width: 250px">
+                                <a href="/More Services" class="btn btn-custom"><i class="mdi mdi-arrow-left ml-2">Web Services</i></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- HOME END-->
+
+{{-- Floating Action Button #Fab--}}
+<x-fab/>
+
+<!-- WELCOME START -->
+<section class="section" id="feature">
+    <div class="container">
+        <div class="row justify-content-center">
+  <div class="col"><p>
+  The VSOFT solution best fits the specific needs and technical requirements of the Mining Industry and addresses the tedious, time consuming, 
+  and costly inefficiencies that comes with using different Software and hardware processes that do not talk to each other.
+<br>
+<br>
+The VSOFT ERP Solution efficiently integrates existing internal mining operations using customized expert software systems that help you grow your business. 
+It offers a futuristic 360 degrees view of all operations with real time data 3D display, with mine business intelligence, 
+using specialised analytic tools and customise reports critical for efficiently managing and expanding mining operations.
+<br>
+<br>
+Engineers are able to see and monitor equipment vitals at any point in a shift. 
+The predictive nature of the algorithms flag imminent failures.
+  </p>
+</div>
+  <div class="col"></div>
+  <div class="col">
+  <div class="mb-4">
+  <img src="/images/about/TechnologyEnt/MobileApp.png" class="img-fluid rounded" alt="">
+</div>
+  </div>
+  <div class="col"><p>
+  Hold all your solutions in the palm of your hand using our unique tool.
+   Use the power of the VSOFT ERP Solution on smart phones and tablets. 
+   This is a service management app for service professionals to automate their workflows. 
+   This solution includes a native mobile app and facilitates up and downstream data propagation from control room to field and vice versa. 
+   actions performed on the mobile application are relayed in the larger application itself and similarly operators and all users are linked to up to date information relating to their area of concern. 
+   The app also HR related information such as employee details and leave balances.
+</p>
+  </div>
+  <div class="w-100"></div>
+  <div class="col"><p>
+  VSOFT Mining Solution provides a Real time accurate and complete easy tracking of management responsibilities. 
+  The system monitors and relays data on key elements such as workers safety, 
+  the mine environment and then self-generates Report Scores that summarizes key information and ensures safety, operations etc. 
+  This enables real-time performance feedback at the end of a customized period � at an operator and crew level.
+  </p></div>
+  <div class="col">
+    <img src="/images/about/TechnologyEnt/truck.png" class="img-fluid rounded" alt="">
+</div>
+  <div class="w-100"></div>
+  <div class="col">
+  <img src="/images/about/TechnologyEnt/Pix.png" class="img-fluid rounded" alt="">
+  </div>
+  <div class="col">
+  <p>
+  The VSOFT integration solution allows you to combine a series of Internet networking devices( IOT) and share data in real time from different systems across different locations. 
+  It comes with an array of native interfaces and is, in fact, an excellent standalone system that allows you to connect to plant equipment, access control systems, and various software applications and beyond.
+  <br>
+  </b>
+  If your coffee machine produces data, VSOFT can talk to it!
+   <br>
+</br>
+  The system integrates algorithms from key mining equipment and operations data collected underground or at the mining face � allowing personnel to get alerts on performance. 
+  A worker gets a detailed report, as well as his team rank, as he leaves work through a 3D interface using 3D Technology. The system also sends real-time operational metrics such as temperature, 
+  vibration, load factors, oil analysis and more to predict by using monitoring algorithms
+  </p>
+  </div>
+</div>
+</div>
+</section>
+<!-- WELCOME END -->
+
+<!-- SERVICES START -->
+
+<!-- SERVICES END -->
+
+<!-- PORTFOLIO START-->
+
+
+        <!-- portfolio menu -->
+
+        <!-- End portfolio  -->
+
+    <!-- Gallary -->
+
+<!-- PORTFOLIO END-->
+
+<!-- CLIENT START -->
+
+<!-- CLIENT END -->
+
+<!-- TEAM START -->
+
+<!-- TEAM END -->
+
+<!-- START CTA -->
+<!-- <section class="section bg-cta">
+    <div class="bg-overlay"></div>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-lg-8">
+                <h3 class="text-white text-center">Ready to start your next
+                    web project now?</h3>
+                <p class="cta-desc text-white text-center mt-3">When a good idea comes, you know, part of my job is to move it around, just see what people think, get people talking about it.</p>
+                <div class="text-center mt-4">
+                    <a href="#" class="btn btn-custom-white">Get Started</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</section> -->
+<!-- END CTA -->
+
+<!-- BLOG START -->
+
+<!-- BLOG END -->
+
+<!-- CONTACT START -->
+
+<!-- CONTACT END -->
+
+<!-- FOOTER START -->
+<section class="footer bg-dark">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-4">
+                <div class="footer-menu">
+                    <h5 class="mb-4 text-uppercase">VSoft Systems</h5>
+                    <p>The Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium eaque quae ab illo inventore veritatis et.</p>
+                    <div class="text-white">
+                        <a href="#facebook"><i class="mdi mdi-facebook facebook footer_mdi"></i></a>
+                        <a href="#twitter"><i class="mdi mdi-twitter twitter footer_mdi"></i></a>
+                        <a href="#google"><i class="mdi mdi-google google footer_mdi"></i></a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-lg-2">
+                <div class="footer-menu">
+                    <h5 class="mb-4 text-uppercase">About</h5>
+                    <ul class="text-muted list-unstyled">
+                        <li><a href="#">About Us</a></li>
+                        <li><a href="#">Press</a></li>
+                        <li><a href="#">Job Opening</a></li>
+                        <li><a href="#">Term</a></li>
+                        <li><a href="#">Privacy</a></li>
+                    </ul>
+                </div>
+            </div>
+
+            <div class="col-lg-2">
+                <div class="footer-menu">
+                    <h5 class="mb-4 text-uppercase">Help Center</h5>
+                    <ul class="text-muted list-unstyled">
+                        <li><a href="#">Accounting</a></li>
+                        <li><a href="#">Billing</a></li>
+                        <li><a href="#">General Question</a></li>
+                    </ul>
+                </div>
+            </div>
+
+            <div class="col-lg-4">
+                <div class="footer-menu">
+                    <h5 class="mb-4 text-uppercase">Contact us</h5>
+                    <p>The Nam eget dui Etiam rhoncus. Maecenas eget sem quam semper libero.</p>
+                    <span class="footer-support">Support@abc.com</span>
+                </div>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="text-center mt-5">
+                    <p class="mb-0">2020  ©  VSoft Systems.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- FOOTER END -->
+
+<!-- JAVASCRIPTS -->
+<script src="js/jquery.min.js"></script>
+<!-- SCROLL -->
+<script src="js/scrollspy.min.js"></script>
+<!-- SCROLL -->
+<script src="js/feather.min.js"></script>
+<!--  -->
+<!-- bootstrap -->
+<script src="js/bootstrap.bundle.min.js"></script>
+<!-- easing -->
+<script src="js/jquery.easing.min.js"></script>
+<!-- Portfolio -->
+<script src="js/jquery.magnific-popup.min.js"></script>
+<script src="js/isotope.js"></script>
+<!-- scroll -->
+<script src="js/scrollspy.min.js"></script>
+<!-- Counter -->
+<script src="js/jquery.counterup.min.js"></script>
+<!-- Owl Carousel -->
+<script src="js/owl.carousel.min.js"></script>
+<!-- Custom -->
+<script src="js/app.js"></script>
+
+</body>
+</html>
