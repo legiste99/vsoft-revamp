7f1f0b2b849ae2d951e818084c8b4819df8410f0
adecel <rustymabiala150@gmail.com>
2023-05-19 22:46:07 +0200

    done with the footer and services_services section

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,7 +1,7 @@
 <section class="footer bg-dark">
     <div class="container">
         <div class="row">
-            <div class="col-lg-4">
+            <div class="col-lg-3">
                 <div class="footer-menu">
                     <h5 class="mb-4 text-uppercase">VSoft Systems</h5>
                     <p>The Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium eaque quae ab illo inventore veritatis et.</p>
@@ -17,41 +17,83 @@
                 <div class="footer-menu">
                     <h5 class="mb-4 text-uppercase">About</h5>
                     <ul class="text-muted list-unstyled">
-                        <li><a href="#">About Us</a></li>
-                        <li><a href="#">Press</a></li>
-                        <li><a href="#">Job Opening</a></li>
-                        <li><a href="#">Term</a></li>
-                        <li><a href="#">Privacy</a></li>
+                        <li><a href="#home">Home</a></li>
+                        <li><a href="#service">Services</a></li>
+                        <li><a href="#portfolio">Portfolio</a></li>
+                        <li><a href="#client">Clients</a></li>
+                        <li><a href="#team">Team</a></li>
+                        <li><a href="#articles">Articles</a></li>
+                        <!-- <li><a href="#price">Store</a></li> -->
+                        <li><a href="#contact">Contact</a></li>
                     </ul>
                 </div>
             </div>
 
-            <div class="col-lg-2">
+
+            <div class="col-lg-3">
                 <div class="footer-menu">
-                    <h5 class="mb-4 text-uppercase">Help Center</h5>
-                    <ul class="text-muted list-unstyled">
-                        <li><a href="#">Accounting</a></li>
-                        <li><a href="#">Billing</a></li>
-                        <li><a href="#">General Question</a></li>
-                    </ul>
+                    <h5 class="mb-4 text-uppercase">Contact us</h5>
+                    <h6 class="text-muted">Email Address</h6>
+                    <p>info@vsoftsystems.co.za</p>
+                    <h6 class="text-muted">Phone number</h6>
+                    <p>(+27)084 249 6846 , (+27)086 579 0155</p>
+                    <h6 class="text-muted">Office Address</h6>
+                    <p>56 Barrack Street, Cape Town, Western Cape, South Africa, ZA-8001</p>
+                    <!-- <p>The Nam eget dui Etiam rhoncus. Maecenas eget sem quam semper libero.</p>
+                    <span class="footer-support">Support@abc.com</span> -->
                 </div>
             </div>
 
             <div class="col-lg-4">
-                <div class="footer-menu">
-                    <h5 class="mb-4 text-uppercase">Contact us</h5>
-                    <p>The Nam eget dui Etiam rhoncus. Maecenas eget sem quam semper libero.</p>
-                    <span class="footer-support">Support@abc.com</span>
-                </div>
+                        <div class="custom-form">
+                            <div id="message"></div>
+                            <form method="post" action="php/contact.php" name="contact-form" id="contact-form">
+                                <div class="row">
+                                    <div class="col-lg-6">
+                                        <div class="form-group">
+                                            <input name="name" id="name" type="text" class="form-control" placeholder="Your name...">
+                                        </div>
+                                    </div>
+                                    <div class="col-lg-6">
+                                        <div class="form-group">
+                                            <input name="email" id="email" type="email" class="form-control" placeholder="Your email...">
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="row">
+                                    <div class="col-lg-12">
+                                        <div class="form-group">
+                                            <input name="text" id="sub" type="text" class="form-control" placeholder="Your subject...">
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="row">
+                                    <div class="col-lg-12">
+                                        <div class="form-group">
+                                            <textarea name="comments" id="comments" rows="4" class="form-control" placeholder="Your message..."></textarea>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="row mt-3">
+                                    <div class="col-lg-12">
+                                        <input type="submit" id="submit" name="send" class="submitBnt btn btn-custom" value="Send Message">
+                                        <div id="simple-msg"></div>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    <!-- </div> -->
             </div>
         </div>
-
+<!-- 
         <div class="row">
             <div class="col-lg-12">
                 <div class="text-center mt-5">
                     <p class="mb-0">2020  ©  VSoft Systems.</p>
                 </div>
             </div>
-        </div>
+        </div> -->
     </div>
 </section>
--- resources/views/components/services_services.blade.php
+++ resources/views/components/services_services.blade.php
@@ -1,76 +1,56 @@
-<section class="section bg-light" id="service">
-    <div class="container">
-        <div class="row mt-4">
-            <div class="col-lg-4">
-                <a href="/components/services-services/search-Engine-Marketing">
-                    <div class="services-blog mt-4">
-                        <div>
-                            <i data-feather="monitor"></i>
-                        </div>
-                        <div class="service-head">
-                            <h4>Search Engine Marketing</h4>
-                            <p class="text-muted mb-0">
-                                Now you say: "Whether you're a small business or a high profile corporation, 
-                                we can offer cutting-edge and strategic marketing services that will promote 
-                                your business on the Internet."
-                            </p>
-                        </div>
-                        <div class="row mt-4">
-                            <div class="col-lg-12">
-                                <div class="text-center mt-5">
-                                    <a href="/components/services-services/search-Engine-Marketing" class="btn btn-custom btn-round">READ MORE</a>
-                                </div>
-                            </div>
+<!-- <section class="section bg-dark" id="service"> -->
+    <!-- <div class="container">
+        <div class="row mt-4"> -->
+
+            <div class="container">
+                <!-- <div class="row justify-content-center">
+                    <div class="col-lg-8">
+                        <div class=" text-center">
+                            <h4 class="title-heading text-uppercase">Testimonial</h4>
+                            <p style="color:orange">We craft digital, graphic and dimensional thinking, to create category leading brand experiences that have meaning and add a value.</p>
                         </div>
                     </div>
-                </a>
-            </div>
-            <div class="col-lg-4">
-                <a href="/components/services-services/mobile-Application-Development">
-                    <div class="services-blog mt-4">
-                        <div>
-                            <i data-feather="smartphone"></i>
-                        </div>
-                        <div class="service-head">
-                            <h4 class="mb-2">Mobile Application Development</h4>
-                            <p class="text-muted mb-0">
-                                Mobile applications can help individuals with quicker information access, 
-                                maximum productivity, and timely responsiveness, there helping him reduce 
-                                operating cost and risks.test
-                            </p>
-                        </div>
-                        <div class="row mt-4">
-                            <div class="col-lg-12">
-                                <div class="text-center mt-5">
-                                    <a href="/components/services-services/mobile-Application-Development" class="btn btn-custom btn-round">READ MORE</a>
+                </div> -->
+
+                <div class="row justify-content-center mt-4">
+                    <div class="col-lg-4">
+                        <div id="owl-demo" class="owl-carousel">
+
+                            <div class="testi-box">
+                                <div class="text-center">
+                                    <div class="mb-4">
+                                        <img src="{{asset('images/services/web_services_images/website_design/image2.jpg')}}" class="img-fluid rounded" alt="">
+                                    </div>
+                                    <h4>Web Maintenance</h4>
+                                    <p class="features-desc text-muted mt-3 pl-2 pr-2">We at Vsoft Systems, with our website maintenance team we will focus on enhancing existing</p>
                                 </div>
                             </div>
-                        </div>
-                    </div>
-                </a>
-            </div>
-            <div class="col-lg-4">
-                <a href="/components/services-services/game-Development">
-                    <div class="services-blog mt-4">
-                        <div>
-                            <i data-feather="smartphone"></i>
-                        </div>
-                        <div class="service-head">
-                            <h4 class="mb-2">Game Development</h4>
-                            <p class="text-muted mb-0">Game development is one of the most intricate parts 
-                                of software development. Unlike ordinary applications, game development 
-                                requires completely updated resources.</p>
-                        </div>
-                        <div class="row mt-4">
-                            <div class="col-lg-12">
-                                <div class="text-center mt-5">
-                                    <a href="/components/services-services/game-Development" class="btn btn-custom btn-round">READ MORE</a>
+
+                            <div class="testi-box">
+                                <div class="text-center">
+                                    <div class="mb-4">
+                                        <img src="{{asset('images/services/web_services_images/website_design/image2.jpg')}}" class="img-fluid rounded" alt="">
+                                    </div>
+                                    <h4>Web Maintenance</h4>
+                                    <p class="features-desc text-muted mt-3 pl-2 pr-2">We at Vsoft Systems, with our website maintenance team we will focus on enhancing existing</p>
                                 </div>
                             </div>
+
+                            <div class="testi-box">
+                                <div class="text-center">
+                                    <div class="mb-4">
+                                        <img src="{{asset('images/services/web_services_images/website_design/image2.jpg')}}" class="img-fluid rounded" alt="">
+                                    </div>
+                                    <h4>Web Maintenance</h4>
+                                    <p class="features-desc text-muted mt-3 pl-2 pr-2">We at Vsoft Systems, with our website maintenance team we will focus on enhancing existing</p>
+                                </div>
+                            </div>
+
                         </div>
                     </div>
-                </a>
+                </div>
             </div>
-        </div>
-    </div>
-</section>
+
+        <!-- </div>
+    </div> -->
+<!-- </section> -->
